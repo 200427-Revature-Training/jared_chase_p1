17b89d9c82f2cf2ac05b39bca8cf9ac33fdba1df
unknown <djaredchase@gmail.com>
2020-06-06 18:51:12 -0400

    updated dao functions

--- server-side/src/daos/reimbursement-dao.ts
+++ server-side/src/daos/reimbursement-dao.ts
@@ -14,7 +14,7 @@ export async function getAllReimb(): Promise<Reimbursement[]> {
     return reimbursements;
 }
 
-export async function getReimbByUser(user: string): Promise<Reimbursement> {
+export async function getAllReimbByUser(user: string): Promise<Reimbursement[]> {
     const employeeExists: boolean = await userExists(user);
     if(!employeeExists){
         return undefined;
@@ -24,9 +24,13 @@ export async function getReimbByUser(user: string): Promise<Reimbursement> {
     join ers_users on ers_reimbursement.reimb_author = ers_users.ers_users_id \
     where user_first_name = $1`;
 
-    //Dont understand the last line here, need to ask for help
-    const result = await db.query<Reimbursement>(sql, [user]);
-    return result.rows.map(row => Reimbursement.from(row))[0];
+    const result = await db.query<ReimbursementRow>(sql, [user]);
+
+    const rows = result.rows;
+    console.log(rows);
+
+    const reimbursements: Reimbursement[] = rows.map(row => Reimbursement.from(row));
+    return reimbursements;
 }
 
 //function to check if user exists and if name spelled correctly
@@ -36,16 +40,51 @@ export async function userExists(firstName: string): Promise<boolean> {
     return result.rows[0].exists;
 }
 
-export async function getReimbByStatus(status: string): Promise<Reimbursement> {
+export async function getAllReimbByStatus(status: string): Promise<Reimbursement[]> {
+    const sql = `select ers_reimbursement.* from ers_reimbursement inner \
+    join ers_reimbursement_status on ers_reimbursement.reimb_status_id = \
+    ers_reimbursement_status.reimb_status_id where reimb_status = $1`;
 
+    const result = await db.query<ReimbursementRow>(sql, [status]);
+
+    const rows = result.rows;
+
+    const reimbursements: Reimbursement[] = rows.map(row => Reimbursement.from(row));
+    return reimbursements;
 }
 
 export async function saveReimb(reimb: Reimbursement): Promise<Reimbursement> {
+    const sql = `insert into ers_reimbursement (reimb_amount, reimb_submitted, reimb_resolved, reimb_description, reimb_receipt, \
+        reimb_author, reimb_resolver, reimb_status_id, reimb_type_id) values 
+        ($1, $2, null, $3, $4, $5, null, $6, $7) returning *`;
 
+    const result = await db.query<ReimbursementRow>(sql, [
+        reimb.reimbAmount,
+        reimb.reimbSubmitted,
+        reimb.reimbDescription,
+        reimb.reimbReceipt,
+        reimb.reimbAuthor,
+        reimb.reimbStatusID,
+        reimb.reimbTypeID
+    ]);
+
+    const rows = result.rows.map(row => Reimbursement.from(row))[0];
+    return rows;
 }
 
 export async function updateReimb(reimb: Reimbursement): Promise<Reimbursement> {
+    const sql = `update ers_reimbursement set reimb_resolved = coalesce($1, reimb_resolved), \
+    set reimb_resolver = coalesce($2, reimb_resolver), set reimb_status_id = coalesce($3, reimb_status_id) \
+    where user_first_name = $4`;
 
+    const resolved = reimb.reimbResolved && reimb.reimbResolved.toISOString();
+
+    const params = [reimb.reimbResolved, reimb.reimbResolver, reimb.reimbStatusID];
+
+    const result = await db.query<ReimbursementRow>(sql, params);
+
+    const rows = result.rows.map(row => Reimbursement.from(row))[0];
+    return rows;
 }
 
 interface Exists {
