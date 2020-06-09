7a6b3e20160555c050000eb34cd05bd6af45ff4f
unknown <djaredchase@gmail.com>
2020-06-09 02:05:49 -0400

    added user-service

--- server-side/src/services/user-service.ts
+++ server-side/src/services/user-service.ts
@@ -1,4 +1,24 @@
 import { User } from '../models/user';
 import * as userDao from '../daos/user-dao';
 
-export function saveUser
+export function getAllUsers(): Promise<User[]> {
+    return userDao.getAllUsers();
+}
+
+export function getUserByUsername(username: string): Promise<User> {
+    return userDao.getUserByUsername(username);
+}
+
+export function saveUser(user: User): Promise<User> {
+    const newUser: User = new User(
+        undefined, user.ersUsername, user.ersPassword, user.userFirstName, 
+        user.userLastName, user.userEmail, user.userRoleID
+    );
+
+    if(user.ersUsername && user.ersPassword && user.userFirstName 
+        && user.userLastName && user.userEmail && user.userRoleID) {
+            return userDao.saveUser(newUser);
+        }else {
+            return new Promise((resolve, reject) => reject(422));
+        }
+}
